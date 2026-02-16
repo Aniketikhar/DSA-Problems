16615729c027a59175322ae3b0f70501eb1b65d9
Aniketikhar <96694643+Aniketikhar@users.noreply.github.com>
2026-02-16 23:08:11 +0530

    pattern 3

---
+++ Pattern-Problems/pattern3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// 1 2 3 4 
+// 5 6 7 8 
+// 9 10 11 12 
+// 13 14 15 16 
+
+
+
+int main() {
+    int n = 5;
+    int count = 1;
+    for(int i = 1 ; i < n; i++){
+        cout << "// " ;
+        for ( int j=1; j < n; j++){
+            cout << count << " ";
+            count++;
+        }
+        cout << endl;
+    }
+    
+    
+    return 0;
+}
