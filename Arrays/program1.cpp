7542ab9dbfbf40f20f881c46f01bbabb69440937
Aniketikhar <96694643+Aniketikhar@users.noreply.github.com>
2026-02-25 15:13:34 +0530

    function and arrays

---
+++ Arrays/program1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void printArray(){
+
+}
+
+int main(){
+    int number[15];
+    int second[] = {1,2,3}
+
+    cout << "Values at 15 index" << number[14]<< endl;
+
+    cout << "Values at 20 index" << number[20]<< endl;
+
+
+    return 0; 
+}
