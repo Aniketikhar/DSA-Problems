20baa4f33dba3d2557c752f85a7bc302a79140ee
Aniketikhar <96694643+Aniketikhar@users.noreply.github.com>
2026-03-01 23:18:34 +0530

    unique no. of occurences

---
+++ Arrays/program4-uniqueNoOfOccurences.cpp
@@ -0,0 +1,43 @@
+// Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+
+// Example 1:
+
+// Input: arr = [1,2,2,1,1,3]
+// Output: true
+// Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+
+
+bool uniqueOccurrences(vector<int>& arr) {
+        vector<int> ans;
+        int size = arr.size();
+
+        sort(arr.begin(), arr.end());
+
+        int i = 0;
+
+        while( i < size){
+            int count = 1;
+            for( int j = i+1; j< size; j++){
+                if(arr[i] == arr[j]){
+                    count++;
+                }else{
+                    break;
+                }
+            }
+            ans.push_back(count);
+            i += count;
+
+        }
+
+        size = ans.size();
+        sort(ans.begin(), ans.end());
+
+        for(int k = 0; k < size - 1; k++){
+            if(ans[k] == ans[k+1]){
+                return false;
+            }
+        }
+
+        return true;
+    }
